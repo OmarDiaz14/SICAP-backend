42cb69bc26926ecfb9cae2223daf0b4fb98f7da5
Omar Diaz <omar2001val@gmail.com>
2026-02-24 14:48:12 -0600

    Update Corte Caja General
    Se cambio corte de caja personal a corte de caja general

--- corte/views.py
+++ corte/views.py
@@ -7,9 +7,11 @@ from rest_framework import status
 from django.shortcuts import get_object_or_404
 
 # Asegúrate de importar tu modelo correctamente
-from cobrador.models import Cobrador 
+#from cobrador.models import Cobrador 
 from .serializers import CorteSerializer
 
+
+"""
 class CorteView(APIView):
     permission_classes = [IsAuthenticated] 
 
@@ -63,6 +65,50 @@ class CorteView(APIView):
             )
 
     def _ejecutar_funcion_corte_db(self, cobrador_id, fecha_inicio, fecha_fin):
+        #Se usa la funcion SQL almacenada y asegura el retorno del JSON 
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT corte_caja(%s, %s, %s);",
+                [fecha_inicio, fecha_fin, cobrador_id]
+            )
+            raw_data = cursor.fetchone()[0]
+
+        if isinstance(raw_data, str):
+            return json.loads(raw_data)
+        return raw_data or {}
+        """
+class CorteView(APIView):
+    # Seguimos protegiendo la ruta: solo usuarios logueados pueden pedir el corte
+    permission_classes = [IsAuthenticated] 
+
+    def post(self, request):
+        serializer = CorteSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True) 
+        fechas = serializer.validated_data
+
+        try:
+            # -----------------------------------------------------------
+            # CORTE GENERAL
+            # -----------------------------------------------------------
+            # Simplemente ejecutamos la función mandando "None" en el cobrador_id.
+            # Tu función SQL está preparada para recibir NULL y sumar todos los pagos.
+            
+            resultado_json = self._ejecutar_funcion_corte_db(
+                fecha_inicio=fechas['fecha_inicio'],  
+                fecha_fin=fechas['fecha_fin'],
+                cobrador_id=None  # <--- MAGIA: Esto hace que sea general
+            )
+
+            return Response(resultado_json, status=status.HTTP_200_OK)
+        
+        except DatabaseError as e:
+            print(f"Error DB: {str(e)}")
+            return Response(
+                {"error": "Error interno de base de datos al generar el corte general."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
+    def _ejecutar_funcion_corte_db(self, fecha_inicio, fecha_fin, cobrador_id):
         """ Se usa la funcion SQL almacenada y asegura el retorno del JSON """
         with connection.cursor() as cursor:
             cursor.execute(
